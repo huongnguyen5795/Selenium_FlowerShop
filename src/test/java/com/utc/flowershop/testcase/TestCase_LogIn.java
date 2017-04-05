3e3c5bbaf1a6ea78ae473c2e74c7815e128ea011
huongnguyen5795 <nguyenthihuong5795@gmail.com>
2017-04-05 22:18:00 +0700

    commit

--- src/test/java/com/utc/flowershop/testcase/TestCase_LogIn.java
+++ src/test/java/com/utc/flowershop/testcase/TestCase_LogIn.java
@@ -10,6 +10,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import com.utc.flowershop.appmodules.LogIn_Action;
 import com.utc.flowershop.pageobject.LogIn_Page;
 import com.utc.flowershop.pageobject.TestBase;
@@ -20,11 +23,14 @@ import com.utc.flowershop.utility.Utility;
 
 public class TestCase_LogIn {
 	public WebDriver driver;
+	public ExtentReports extent;
+	public ExtentTest test;
 
 	@BeforeMethod
 	public void beforeMethod() throws Exception {
 		DOMConfigurator.configure("log4j.xml");
 		Log.startTestCase("LogIn");
+		extent = new ExtentReports("D:/Nam4ky2/NCKH/Code/selenium/DoAn_Selenium/Reports/TestReport_SignIn.html", true);
 		ExcelUtils.setExcelFile(Constant.pathTestData + Constant.fileTestData, "LogIn");
 		Log.info("Excel sheet opened");
 		String browserName = ExcelUtils.getCellData(1, 5);
@@ -43,15 +49,21 @@ public class TestCase_LogIn {
 	@Test(priority = 1)
 	public void signIn_TC001() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC001");
 			LogIn_Action.testCase001();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.PASS, "Hiển thị thông báo khi nhập Tên Đăng Nhập > 30 ký tự ");
 				LogIn_Page.test_pass(1);
 			} else {
-				LogIn_Page.test_failed(1, "Không hiển thị thông báo khi nhập quá độ dài Tên ĐN");
+				test.log(LogStatus.FAIL, "Không hiển thị thông báo khi nhập Tên Đăng Nhập > 30 ký tự ");
+				LogIn_Page.test_failed(1, "Không hiển thị thông báo khi nhập  Tên ĐN > 30 ký tự");
+
 			}
+			extent.endTest(test);
+	    	extent.flush();
 		} catch (Exception e) {
-			LogIn_Page.test_failed(1,"Error Exception");
+			LogIn_Page.test_failed(1, "Error Exception");
 			e.printStackTrace();
 		}
 	}
@@ -59,13 +71,18 @@ public class TestCase_LogIn {
 	@Test(priority = 2)
 	public void signIn_TC002() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC002");
 			LogIn_Action.testCase002();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.PASS, "Hiển thị thông báo khi nhập Tên ĐN = 30 ký tự chưa có trong CSDL ");
 				LogIn_Page.test_pass(2);
 			} else {
+				test.log(LogStatus.FAIL, "Không Hiển thị thông báo khi nhập Tên ĐN = 30 ký tự chưa có trong CSDL ");
 				LogIn_Page.test_failed(2, "Không hiển thị thông báo khi nhập Tên ĐN = 30 ký tự chưa có trong CSDL");
 			}
+			extent.endTest(test);
+	    	extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(2, "Error exception");
 			e.printStackTrace();
@@ -75,16 +92,19 @@ public class TestCase_LogIn {
 	@Test(priority = 3)
 	public void signIn_TC003() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC003");
 			LogIn_Action.testCase003();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.PASS, "Hiển thị thông báo khi nhập Tên ĐN < 30 ký tự chưa có trong CSDL");
 				LogIn_Page.test_pass(3);
 			} else {
+				test.log(LogStatus.FAIL, "Không hiển thị thông báo khi nhập Tên ĐN < 30 ký tự chưa có trong CSDL");
 				LogIn_Page.test_failed(3, "Không hiển thị thông báo khi nhập Tên ĐN < 30 ký tự chưa có trong CSDL");
 			}
-		} catch (
-
-		Exception e) {
+			extent.endTest(test);
+	    	extent.flush();
+		} catch (Exception e) {
 			LogIn_Page.test_failed(3, "Error exception");
 			e.printStackTrace();
 		}
@@ -93,16 +113,19 @@ public class TestCase_LogIn {
 	@Test(priority = 4)
 	public void signIn_TC004() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC004");
 			LogIn_Action.testCase004();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.PASS, "Hiển thị thông báo khi không Tên ĐN");
 				LogIn_Page.test_pass(4);
 			} else {
+				test.log(LogStatus.FAIL, "Không hiển thị thông báo khi không Tên ĐN");
 				LogIn_Page.test_failed(4, "Không hiển thị thông báo khi không Tên ĐN");
 			}
-		} catch (
-
-		Exception e) {
+			extent.endTest(test);
+			extent.flush();
+		} catch (Exception e) {
 			LogIn_Page.test_failed(4, "Error exception");
 			e.printStackTrace();
 		}
@@ -111,16 +134,19 @@ public class TestCase_LogIn {
 	@Test(priority = 5)
 	public void signIn_TC005() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC005");
 			LogIn_Action.testCase005();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.PASS, "Hiển thị thông báo khi nhập Mật khẩu > 30 ký tự");
 				LogIn_Page.test_pass(5);
 			} else {
+				test.log(LogStatus.FAIL, "Không hiển thị thông báo khi nhập Mật khẩu > 30 ký tự");
 				LogIn_Page.test_failed(5, "Không hiển thị thông báo khi nhập Mật khẩu > 30 ký tự ");
 			}
-		} catch (
-
-		Exception e) {
+			extent.endTest(test);
+			extent.flush();
+		} catch (Exception e) {
 			LogIn_Page.test_failed(5, "Error exception");
 			e.printStackTrace();
 		}
@@ -129,17 +155,20 @@ public class TestCase_LogIn {
 	@Test(priority = 6)
 	public void signIn_TC006() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC006");
 			LogIn_Action.testCase006();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.FAIL, "Không hiển thị thông báo khi nhập Mật khẩu > 30 ký tự");
 				LogIn_Page.test_pass(6);
 			} else {
+				test.log(LogStatus.FAIL, "Không hiển thị thông báo khi nhập Mật khẩu > 30 ký tự");
 				LogIn_Page.test_failed(6,
 						"Không hiển thị thông báo khi nhập Tên ĐN = 30 ký tự có trong CSDL, không nhập Mật khẩu ");
 			}
-		} catch (
-
-		Exception e) {
+			extent.endTest(test);
+			extent.flush();
+		} catch (Exception e) {
 			LogIn_Page.test_failed(6, "Error exception");
 			e.printStackTrace();
 		}
@@ -148,17 +177,22 @@ public class TestCase_LogIn {
 	@Test(priority = 7)
 	public void signIn_TC007() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC007");
 			LogIn_Action.testCase007();
 			if (LogIn_Page.message().getText().equals("Sai Tên Đăng Nhập Hoặc Mật Khẩu")) {
 				Log.info("Check display message form SignIn ");
+				test.log(LogStatus.PASS,
+						"Hiển thị thông báo khi nhập Tên ĐN < 30 ký tự có trong CSDL, không nhập Mật khẩu");
 				LogIn_Page.test_pass(7);
 			} else {
+				test.log(LogStatus.FAIL,
+						"Không hiển thị thông báo khi nhập Tên ĐN < 30 ký tự có trong CSDL, không nhập Mật khẩu");
 				LogIn_Page.test_failed(7,
 						"Không hiển thị thông báo khi nhập Tên ĐN < 30 ký tự có trong CSDL, không nhập Mật khẩu ");
 			}
-		} catch (
-
-		Exception e) {
+			extent.endTest(test);
+			extent.flush();
+		} catch (Exception e) {
 			LogIn_Page.test_failed(7, "Error exception");
 			e.printStackTrace();
 		}
@@ -167,14 +201,21 @@ public class TestCase_LogIn {
 	@Test(priority = 8)
 	public void signIn_TC008() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC008");
 			LogIn_Action.testCase008();
 			if (!LogIn_Page.message().isDisplayed()) {
 				Log.info("Check display link thong tin tai khoan form SignIn ");
+				test.log(LogStatus.PASS,
+						"Đăng nhập không thành công khi tên tài khoản , mật khẩu = 30 ký tự có trong CSDL");
 				LogIn_Page.test_pass(8);
 			} else {
+				test.log(LogStatus.FAIL,
+						"Đăng nhập không thành công khi tên tài khoản , mật khẩu = 30 ký tự có trong CSDL");
 				LogIn_Page.test_failed(8,
-						"Đăng nhập không thành công khi tên tai khoan khong co trong CSDL || Đăng ký giới hạn độ dài Tên ĐN , Pass");
+						"Đăng nhập không thành công khi tên tài khoản , mật khẩu = 30 ký tự có trong CSDL ");
 			}
+			extent.endTest(test);
+			extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(8, "Error exception");
 			e.printStackTrace();
@@ -184,14 +225,21 @@ public class TestCase_LogIn {
 	@Test(priority = 9)
 	public void signIn_TC009() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC009");
 			LogIn_Action.testCase009();
 			if (!LogIn_Page.message().isDisplayed()) {
 				Log.info("Check display link thong tin tai khoan form SignIn ");
+				test.log(LogStatus.PASS,
+						"Đăng nhập thành công khi tên tài khoản < 30 ký tự có trong CSDL , mật khẩu = 30 ký tự có trong CSDL");
 				LogIn_Page.test_pass(9);
 			} else {
+				test.log(LogStatus.FAIL,
+						"Đăng nhập không thành công khi tên tài khoản < 30 ký tự có trong CSDL , mật khẩu = 30 ký tự có trong CSDL");
 				LogIn_Page.test_failed(9,
-						"Đăng nhập không thành công khi tên tai khoan khong co trong CSDL || Đăng ký giới hạn độ dài Tên ĐN , Pass");
+						"Đăng nhập không thành công khi tên tài khoản < 30 ký tự có trong CSDL , mật khẩu = 30 ký tự có trong CSDL");
 			}
+			extent.endTest(test);
+			extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(9, "Error exception");
 			e.printStackTrace();
@@ -201,14 +249,21 @@ public class TestCase_LogIn {
 	@Test(priority = 10)
 	public void signIn_TC010() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC010");
 			LogIn_Action.testCase010();
 			if (!LogIn_Page.message().isDisplayed()) {
 				Log.info("Check display link thong tin tai khoan form SignIn ");
+				test.log(LogStatus.PASS,
+						"Đăng nhập thành công khi tên tài khoản < 30 ký tự có trong CSDL , mật khẩu = 30 ký tự có trong CSDL");
 				LogIn_Page.test_pass(10);
 			} else {
+				test.log(LogStatus.FAIL,
+						"Đăng nhập không thành công khi tên tài khoản < 30 ký tự có trong CSDL , mật khẩu = 30 ký tự có trong CSDL");
 				LogIn_Page.test_failed(10,
-						"Đăng nhập không thành công khi tên tai khoan khong co trong CSDL || Đăng ký giới hạn độ dài Tên ĐN , Pass");
+						"Đăng nhập không thành công khi tên tài khoản = 30 ký tự có trong CSDL , mật khẩu < 30 ký tự có trong CSDL");
 			}
+			extent.endTest(test);
+			extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(10, "Error exception");
 			e.printStackTrace();
@@ -218,15 +273,22 @@ public class TestCase_LogIn {
 	@Test(priority = 11)
 	public void signIn_TC011() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC011");
 			LogIn_Action.testCase011();
 			Thread.sleep(5000);
 			if (LogIn_Page.link_thongTinTK().isDisplayed()) {
 				Log.info("Check display link thong tin tai khoan form SignIn ");
+				test.log(LogStatus.PASS, "Đăng nhập thành công khi tên tài khoản, mật khấu < 30 ký tự có trong CSDL ");
+
 				LogIn_Page.test_pass(11);
 			} else {
+				test.log(LogStatus.FAIL,
+						"Đăng nhập không thành công khi tên tài khoản, mật khấu < 30 ký tự có trong CSDL ");
 				LogIn_Page.test_failed(11,
-						"Đăng nhập không thành công khi tên tai	 khoan khong co trong CSDL || Đăng ký giới hạn độ dài Tên ĐN , Pass");
+						"Đăng nhập không thành công khi tên tài khoản, mật khấu < 30 ký tự có trong CSDL ");
 			}
+			extent.endTest(test);
+			extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(11, "Error exception");
 			e.printStackTrace();
@@ -236,19 +298,24 @@ public class TestCase_LogIn {
 	@Test(priority = 12)
 	public void signIn_TC012() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC012");
 			if (LogIn_Page.link_dangKy().isDisplayed()) {
 				LogIn_Page.link_dangKy().click();
 				Log.info("Click Link Dang Ky");
 				String titleDangKy = driver.getTitle();
 				Log.info("Get title Dang ky");
 				if (titleDangKy.equals("dangky")) {
+					test.log(LogStatus.PASS, "Chuyển sang giao diên đăng ký");
 					LogIn_Page.test_pass(12);
 					Log.info("Check successfull link Dang ky");
 				} else {
+					test.log(LogStatus.FAIL, "Không chuyển sang giao diên đăng ký");
 					LogIn_Page.test_failed(12, "Không chuyển sang giao diên đăng ký");
 					Log.error("Check not successfull link Dang ky");
 				}
 			}
+			extent.endTest(test);
+			extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(12, "Error exception");
 			e.printStackTrace();
@@ -258,20 +325,25 @@ public class TestCase_LogIn {
 	@Test(priority = 13)
 	public void signIn_TC013() throws Exception {
 		try {
+			test = extent.startTest("SignIn_TC013");
 			if (LogIn_Page.link_foundPass().isDisplayed()) {
 				LogIn_Page.link_foundPass().click();
 				Log.info("Click Link quen mat khau");
 				String titleTimPass = driver.getTitle();
 				Log.info("Get title quyen mat khau");
 				if (titleTimPass.equals("timmatkhau")) {
+					test.log(LogStatus.PASS, "Chuyển sang giao diên quên mật khẩu");
 					LogIn_Page.test_pass(13);
 					Log.info("Check successfull link quen pass");
 
 				} else {
+					test.log(LogStatus.FAIL, "Không chuyển sang giao diên quên mật khẩu");
 					LogIn_Page.test_failed(13, "Không chuyển sang giao diên quên mật khẩu");
 					Log.error("Check not successfull link quen pass");
 				}
 			}
+			extent.endTest(test);
+			extent.flush();
 		} catch (Exception e) {
 			LogIn_Page.test_failed(13, "Error exception");
 			e.printStackTrace();
