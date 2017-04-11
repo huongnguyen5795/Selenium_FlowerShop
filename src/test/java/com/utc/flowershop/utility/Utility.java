a9f18eae2432ccd05c9c5f660baf577aed80af6f
huongnguyen5795 <nguyenthihuong5795@gmail.com>
2017-04-11 15:54:05 +0700

    commit

--- src/test/java/com/utc/flowershop/utility/Utility.java
+++ src/test/java/com/utc/flowershop/utility/Utility.java
@@ -9,8 +9,12 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.apache.commons.io.FileUtils;
 
 public class Utility {
@@ -29,6 +33,14 @@ public class Utility {
 				driver.get(Constant.url);
 				Log.info("Web application launched successfully");
 			}
+			 else if (browserName.equals("chrome") || browserName.equals("CHROME")) {
+					System.setProperty("webdriver.chrome.driver","D:\\cai win\\chromedriver_win32\\chromedriver.exe");
+					driver = new ChromeDriver();
+					driver.manage().window().maximize();
+				} else if (browserName.equals("ie") || browserName.equals("IE")) {
+					driver = new InternetExplorerDriver();
+					driver.manage().window().maximize();
+				}
 		} catch (Exception e) {
 			Log.error("Class Utils | Method OpenBrowser | Exception desc : " + e.getMessage());
 		}
